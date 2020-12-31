d76ce12f4719fc6a7e4e74b959dc14208330129e
Rayliu <rayliu10k@gmail.com>
2020-12-31 15:34:41 +0800

    第一次提交

---
+++ cmake-build-debug/CMakeFiles/CPP_Learning_github.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Learning_github.dir/变量.cpp.o"
+  "CPP_Learning_github"
+  "CPP_Learning_github.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Learning_github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
