bf48b0b783d410c017c77fc0804c9fdca360f508
Rayliu <rayliu10k@gmail.com>
2021-03-21 11:00:15 +0800

    温度转换

---
+++ practice/CCF/P1001.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Rayliu40k on 2021/3/21.
+//
+
+#include "P1001.h"
+
+void P1001() {
+    cout << "Please input number:" << endl;
+    float F;
+    cin >> F;
+    float C;
+    C = (F - 32) * 5 / 9;
+    cout << "Number is:" <<  fixed << setprecision(4) << C ;
+}
---
+++ practice/CCF/P1001.h
@@ -0,0 +1,16 @@
+//
+// Created by Rayliu40k on 2021/3/21.
+//
+
+#ifndef CPP_LEARNING_GITHUB_P1001_H
+#define CPP_LEARNING_GITHUB_P1001_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void P1001();
+
+#endif //CPP_LEARNING_GITHUB_P1001_H
